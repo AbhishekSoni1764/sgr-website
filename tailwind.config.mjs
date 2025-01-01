02d0efe0d026dc78b5b7b1b0da83f76101902b73
Abhishek Soni <abhisoni1764@gmail.com>
2025-01-02 00:01:16 +0530

    Add utility functions, custom components, and update styles for SGR branding

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,48 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
+    fontFamily: {
+      barlow: ["--var(--font-barlow)"],
+      barlow_condensed: ["--var(--font-barlow-condensed)"],
+      poppins: ["--var(--font-poppins)"],
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        secondary: {
+          800: "#09803D",
+          200: "#EAF7ED",
+        },
+        primary: {
+          800: "##FFFFFF",
+          200: "#C4C4C4",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
