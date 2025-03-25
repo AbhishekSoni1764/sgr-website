b67bad1a8223b9ec5936715b2fbc4b1f463929e9
Abhishek Soni <abhisoni1764@gmail.com>
2025-03-26 02:49:14 +0530

    Remove deprecated Lenis packages, update package dependencies, and add ApartSection component

---
+++ src/components/Apart.jsx
@@ -0,0 +1,11 @@
+const ApartSection = () => {
+    return (
+        <div className="h-screen bg-slate-300 w-full flex flex-col justify-center items-center">
+            <div>
+                Apart Section
+            </div>
+        </div>
+    );
+};
+
+export default ApartSection;
